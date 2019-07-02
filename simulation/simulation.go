1d815f8e0a8e338b5f0e5b072e7e351d7465069b
Dominick Triola <dominicktriola@gmail.com>
2019-07-01 23:53:17 -0700

    Generate RGBA image rather than paletted

--- simulation/simulation.go
+++ simulation/simulation.go
@@ -1,35 +1,36 @@
 package simulation
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"os"
 )
 
-// Build creates an automata simulation as a GIF
+// Build creates an automata simulation as a GIF (WIP)
 func Build() (name string) {
-	fmt.Println("This function will generate an animated automata simulation, but so far it only creates a boring image.")
-	name = "static/image.png"
+	name = "tmp/image.png"
 	makeImage(name)
 	return name
 }
 
-var palette = []color.Color{
-	color.RGBA{0x00, 0x00, 0x00, 0xff},
-}
-
 func makeImage(name string) {
 	const (
-		x      = 0
-		y      = 0
+		x0     = 0
+		y0     = 0
 		width  = 100
 		height = 100
 	)
 
-	rect := image.Rect(x, y, width, height)
-	img := image.NewPaletted(rect, palette)
+	rect := image.Rect(x0, y0, width, height)
+	img := image.NewRGBA(rect)
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.SetRGBA(x, y, color.RGBA{255, 0, 0, 255})
+		}
+	}
+
 	f, _ := os.Create(name)
 	png.Encode(f, img)
 }
