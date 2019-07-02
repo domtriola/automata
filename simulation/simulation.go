7a5b04f77c4105f9c16dbd3df325216b179a600c
Dominick Triola <dominicktriola@gmail.com>
2019-07-02 00:46:40 -0700

    Generate gif that cycles through all Plan9 palette colors

--- simulation/simulation.go
+++ simulation/simulation.go
@@ -2,35 +2,63 @@ package simulation
 
 import (
 	"image"
-	"image/color"
-	"image/png"
+	"image/color/palette"
+	"image/gif"
 	"os"
 )
 
 // Build creates an automata simulation as a GIF (WIP)
 func Build() (name string) {
-	name = "tmp/image.png"
-	makeImage(name)
+	name = "tmp/image.gif"
+	animate(name)
 	return name
 }
 
-func makeImage(name string) {
-	const (
-		x0     = 0
-		y0     = 0
-		width  = 100
-		height = 100
-	)
+type grid struct {
+	width  int
+	height int
+	cells  []cell
+}
 
-	rect := image.Rect(x0, y0, width, height)
-	img := image.NewRGBA(rect)
+type cell struct {
+	x         int
+	y         int
+	state     int8
+	neighbors []cell
+}
 
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			img.SetRGBA(x, y, color.RGBA{255, 0, 0, 255})
-		}
+const (
+	x0      = 0
+	y0      = 0
+	width   = 500
+	height  = 500
+	nFrames = 256
+	delay   = 10
+)
+
+func animate(name string) {
+	anim := gif.GIF{LoopCount: nFrames}
+
+	for i := 0; i < nFrames; i++ {
+		img := createImage(uint8(i))
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
 
 	f, _ := os.Create(name)
-	png.Encode(f, img)
+	gif.EncodeAll(f, &anim)
+}
+
+func createImage(colorIndex uint8) (img *image.Paletted) {
+	rect := image.Rect(x0, y0, width, height)
+	img = image.NewPaletted(rect, palette.Plan9)
+
+	// Generate image with random color
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.SetColorIndex(x, y, colorIndex)
+		}
+	}
+
+	return img
 }
