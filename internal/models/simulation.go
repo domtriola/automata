fc71693776a9cb1cd8807f9c19a038a4f217cacf
Dominick Triola <dominicktriola@gmail.com>
2020-04-06 00:22:46 -0700

    Create options

--- internal/models/simulation.go
+++ internal/models/simulation.go
@@ -6,3 +6,28 @@ type Simulation interface {
 	InitializeGrid() *Grid
 	CalculateNextFrame(g *Grid) error
 }
+
+// Options are the configurations for the simulation
+type Options struct {
+	// Width sets the width of the grid
+	Width int
+
+	// Height sets the height of the grid
+	Height int
+
+	// NFrames is the amount of frames that will be built in the GIF
+	NFrames int
+
+	CellularAutomataOptions
+	SlimeMoldOptions
+}
+
+// CellularAutomataOptions are options specific to the cellular automata
+// simulation
+type CellularAutomataOptions struct{}
+
+// SlimeMoldOptions are options specific to the cellular automata simulation
+type SlimeMoldOptions struct {
+	ScentDecay        float32
+	ScentSpreadFactor float32
+}
