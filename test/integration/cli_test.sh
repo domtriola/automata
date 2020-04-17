5b2664e219bbe450f009467d7e0e01e65da81886
Dominick Triola <dominicktriola@gmail.com>
2020-04-16 19:24:08 -0700

    Update integration tests

--- test/integration/cli_test.sh
+++ test/integration/cli_test.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-OUTPUT_PATH="test/tmp/test_sim.gif"
-
 rm -f test/tmp/*.gif
+
+echo "it creates a basic simulation"
+
+OUTPUT_PATH="test/tmp/test_sim.gif"
 
 go run cmd/cli/main.go --out $OUTPUT_PATH
 
@@ -12,3 +14,23 @@ if [ ! -f $OUTPUT_PATH ]; then
   echo "$OUTPUT_PATH not found"
   exit 1
 fi
+
+echo "ok"
+echo "--------"
+
+echo "it creates a simulation with custom params"
+
+OUTPUT_PATH="test/tmp/custom_params.gif"
+
+go run cmd/cli/main.go --out $OUTPUT_PATH --width 4 --height 10 --nFrames 2 --nSpecies 2 pThreshold 2 pDirs "n,e,w,s"
+
+if [ ! -f $OUTPUT_PATH ]; then
+  echo "$OUTPUT_PATH not found"
+  exit 1
+fi
+
+echo "ok"
+echo "--------"
+
+echo "cli tests pass"
+echo "----------------------"
--- test/integration/run.sh
+++ test/integration/run.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+echo "Running integration tests"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/cli_test.sh
