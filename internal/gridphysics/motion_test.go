e415a4985d8e732b8cee8ab63b1854cdf6a3d9e2
Dominick Triola <dominicktriola@gmail.com>
2020-04-23 23:39:06 -0700

    Add motion tests

--- internal/gridphysics/motion_test.go
+++ internal/gridphysics/motion_test.go
@@ -13,14 +13,14 @@ func TestCoordinate(t *testing.T) {
 	t.Run("Move() returns the correct coordinate", func(t *testing.T) {
 		t.Parallel()
 
-		type testCases struct {
+		type testCase struct {
 			name     string
 			coord    gridphysics.Coordinate
 			vect     gridphysics.AngleVector
 			expected gridphysics.Coordinate
 		}
 
-		tCases := []testCases{
+		testCases := []testCase{
 			{
 				name:     "no movement",
 				coord:    gridphysics.Coordinate{0, 0},
@@ -71,8 +71,8 @@ func TestCoordinate(t *testing.T) {
 			},
 		}
 
-		for _, testCase := range tCases {
-			tc := testCase
+		for _, tCase := range testCases {
+			tc := tCase
 
 			t.Run(tc.name, func(t *testing.T) {
 				t.Parallel()
@@ -80,6 +80,72 @@ func TestCoordinate(t *testing.T) {
 				nextCoord := tc.coord.Move(tc.vect)
 				assert.InDelta(t, tc.expected[0], nextCoord[0], 0.00001, "unexpected x coord")
 				assert.InDelta(t, tc.expected[1], nextCoord[1], 0.00001, "unexpected y coord")
+			})
+		}
+	})
+
+	t.Run("ToDiscreteCoordinate() returns the correct coordinate", func(t *testing.T) {
+		t.Parallel()
+
+		type testCase struct {
+			name     string
+			coord    gridphysics.Coordinate
+			expected gridphysics.DiscreteCoord
+		}
+
+		testCases := []testCase{
+			{name: "zero", coord: gridphysics.Coordinate{0, 0}, expected: gridphysics.DiscreteCoord{0, 0}},
+		}
+
+		for _, tCase := range testCases {
+			tc := tCase
+
+			t.Run(tc.name, func(t *testing.T) {
+				dCoord := tc.coord.ToDiscreteCoordinate()
+
+				assert.Equal(t, tc.expected[0], dCoord[0], "unexpected x coordinate")
+				assert.Equal(t, tc.expected[1], dCoord[1], "unexpected y coordinate")
+			})
+		}
+	})
+}
+
+func TestVector(t *testing.T) {
+	t.Parallel()
+
+	t.Run("LinearVelocity() returns the correct values", func(t *testing.T) {
+		t.Parallel()
+
+		type testCase struct {
+			name     string
+			vect     gridphysics.AngleVector
+			expected [2]float64
+		}
+
+		testCases := []testCase{
+			{name: "zero", vect: gridphysics.AngleVector{0, 0}, expected: [2]float64{0, 0}},
+			{
+				name:     "up",
+				vect:     gridphysics.AngleVector{gridphysics.DegreeAngle(90).ToRadians(), 1},
+				expected: [2]float64{0, 1},
+			},
+			{
+				name:     "over",
+				vect:     gridphysics.AngleVector{gridphysics.DegreeAngle(36.87).ToRadians(), 5},
+				expected: [2]float64{4, 3},
+			},
+		}
+
+		for _, tCase := range testCases {
+			tc := tCase
+
+			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
+
+				x, y := tc.vect.LinearVelocity()
+
+				assert.InDelta(t, tc.expected[0], x, 0.0001, "unexpected x coordinate")
+				assert.InDelta(t, tc.expected[1], y, 0.0001, "unexpected y coordinate")
 			})
 		}
 	})
