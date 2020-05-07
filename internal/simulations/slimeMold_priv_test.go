28b8b78a0a85d1bf32cd067dc214d5e2c1283a1e
Dominick Triola <dominicktriola@gmail.com>
2020-05-06 21:31:01 -0700

    Skip tests that aren't written yet

--- internal/simulations/slimeMold_priv_test.go
+++ internal/simulations/slimeMold_priv_test.go
@@ -2,18 +2,12 @@ package simulations
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSlimeMoldPrivateMethods(t *testing.T) {
 	t.Parallel()
 
-	t.Run("SlimeMold.calculateNextFrame() sets appropriate next positions", func(t *testing.T) {
-		assert.True(t, true)
-	})
+	t.Skip("SlimeMold.calculateNextFrame() sets appropriate next positions")
 
-	t.Run("SlimeMold.applyNextFrame() applies appropriate next positions", func(t *testing.T) {
-		assert.True(t, true)
-	})
+	t.Skip("SlimeMold.applyNextFrame() applies appropriate next positions")
 }
